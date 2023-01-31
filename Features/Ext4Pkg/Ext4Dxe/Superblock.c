riscvarchive/riscv-edk2-platforms

commit d9ceedca6c8f4110c73489a99ee74b371fca63fd

Ext4Pkg: Add Ext4Dxe driver.
Adds a UEFI EXT4 filesystem driver that implements the EFI_FILE_PROTOCOL
and EFI_SIMPLE_FILE_SYSTEM_PROTOCOL.

Cc: Leif Lindholm <leif@nuviainc.com>
Cc: Michael D Kinney <michael.d.kinney@intel.com>
Cc: Bret Barkelew <Bret.Barkelew@microsoft.com>
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>
Reviewed-by: Michael D Kinney <michael.d.kinney@intel.com>

Features/Ext4Pkg/Ext4Dxe/Superblock.c
@@ -1,0 +1,297 @@
+/** @file
+  Superblock managing routines
+
+  Copyright (c) 2021 Pedro Falcato All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include "Ext4Dxe.h"
+
+STATIC CONST UINT32  gSupportedCompatFeat = EXT4_FEATURE_COMPAT_EXT_ATTR;
+
+STATIC CONST UINT32  gSupportedRoCompatFeat =
+  EXT4_FEATURE_RO_COMPAT_DIR_NLINK | EXT4_FEATURE_RO_COMPAT_EXTRA_ISIZE |
+  EXT4_FEATURE_RO_COMPAT_HUGE_FILE | EXT4_FEATURE_RO_COMPAT_LARGE_FILE |
+  EXT4_FEATURE_RO_COMPAT_GDT_CSUM | EXT4_FEATURE_RO_COMPAT_METADATA_CSUM | EXT4_FEATURE_RO_COMPAT_SPARSE_SUPER;
+
+STATIC CONST UINT32  gSupportedIncompatFeat =
+  EXT4_FEATURE_INCOMPAT_64BIT | EXT4_FEATURE_INCOMPAT_DIRDATA |
+  EXT4_FEATURE_INCOMPAT_FLEX_BG | EXT4_FEATURE_INCOMPAT_FILETYPE |
+  EXT4_FEATURE_INCOMPAT_EXTENTS | EXT4_FEATURE_INCOMPAT_LARGEDIR |
+  EXT4_FEATURE_INCOMPAT_MMP;
+
+// Future features that may be nice additions in the future:
+// 1) Btree support: Required for write support and would speed up lookups in large directories.
+// 2) meta_bg: Required to mount meta_bg-enabled partitions.
+
+// Note: We ignore MMP because it's impossible that it's mapped elsewhere,
+// I think (unless there's some sort of network setup where we're accessing a remote partition).
+
+// Note on corruption signaling:
+// We (Ext4Dxe) could signal corruption by setting s_state to |= EXT4_FS_STATE_ERRORS_DETECTED.
+// I've decided against that, because right now the driver is read-only, and
+// that would mean we would need to writeback the superblock. If something like
+// this is desired, it's fairly trivial to look for EFI_VOLUME_CORRUPTED
+// references and add some Ext4SignalCorruption function + function call.
+
+/**
+   Does brief validation of the ext4 superblock.
+
+   @param[in] Sb     Pointer to the read superblock.
+
+   @return TRUE if a valid ext4 superblock, else FALSE.
+**/
+BOOLEAN
+Ext4SuperblockValidate (
+  CONST EXT4_SUPERBLOCK  *Sb
+  )
+{
+  if (Sb->s_magic != EXT4_SIGNATURE) {
+    return FALSE;
+  }
+
+  if (Sb->s_rev_level != EXT4_DYNAMIC_REV && Sb->s_rev_level != EXT4_GOOD_OLD_REV) {
+    return FALSE;
+  }
+
+  // Is this correct behaviour? Imagine the power cuts out, should the system fail to boot because
+  // we're scared of touching something corrupt?
+  if ((Sb->s_state & EXT4_FS_STATE_UNMOUNTED) == 0) {
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+/**
+   Calculates the superblock's checksum.
+
+   @param[in] Partition    Pointer to the opened partition.
+   @param[in] Sb           Pointer to the superblock.
+
+   @return The superblock's checksum.
+**/
+STATIC
+UINT32
+Ext4CalculateSuperblockChecksum (
+  EXT4_PARTITION         *Partition,
+  CONST EXT4_SUPERBLOCK  *Sb
+  )
+{
+  // Most checksums require us to go through a dummy 0 as part of the requirement
+  // that the checksum is done over a structure with its checksum field = 0.
+  UINT32  Checksum;
+
+  Checksum = Ext4CalculateChecksum (
+               Partition,
+               Sb,
+               OFFSET_OF (EXT4_SUPERBLOCK, s_checksum),
+               ~0U
+               );
+
+  return Checksum;
+}
+
+/**
+   Verifies that the superblock's checksum is valid.
+
+   @param[in] Partition    Pointer to the opened partition.
+   @param[in] Sb           Pointer to the superblock.
+
+   @return The superblock's checksum.
+**/
+STATIC
+BOOLEAN
+Ext4VerifySuperblockChecksum (
+  EXT4_PARTITION         *Partition,
+  CONST EXT4_SUPERBLOCK  *Sb
+  )
+{
+  if (!EXT4_HAS_METADATA_CSUM (Partition)) {
+    return TRUE;
+  }
+
+  return Sb->s_checksum == Ext4CalculateSuperblockChecksum (Partition, Sb);
+}
+
+/**
+   Opens and parses the superblock.
+
+   @param[out]     Partition Partition structure to fill with filesystem details.
+   @retval EFI_SUCCESS       Parsing was succesful and the partition is a
+                             valid ext4 partition.
+**/
+EFI_STATUS
+Ext4OpenSuperblock (
+  OUT EXT4_PARTITION  *Partition
+  )
+{
+  UINT32                 Index;
+  EFI_STATUS             Status;
+  EXT4_SUPERBLOCK        *Sb;
+  UINT32                 NrBlocksRem;
+  UINTN                  NrBlocks;
+  UINT32                 UnsupportedRoCompat;
+  EXT4_BLOCK_GROUP_DESC  *Desc;
+
+  Status = Ext4ReadDiskIo (
+             Partition,
+             &Partition->SuperBlock,
+             sizeof (EXT4_SUPERBLOCK),
+             EXT4_SUPERBLOCK_OFFSET
+             );
+
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  Sb = &Partition->SuperBlock;
+
+  if (!Ext4SuperblockValidate (Sb)) {
+    return EFI_VOLUME_CORRUPTED;
+  }
+
+  if (Sb->s_rev_level == EXT4_DYNAMIC_REV) {
+    Partition->FeaturesCompat   = Sb->s_feature_compat;
+    Partition->FeaturesIncompat = Sb->s_feature_incompat;
+    Partition->FeaturesRoCompat = Sb->s_feature_ro_compat;
+    Partition->InodeSize = Sb->s_inode_size;
+  } else {
+    // GOOD_OLD_REV
+    Partition->FeaturesCompat = Partition->FeaturesIncompat = Partition->FeaturesRoCompat = 0;
+    Partition->InodeSize = EXT4_GOOD_OLD_INODE_SIZE;
+  }
+
+  // Now, check for the feature set of the filesystem
+  // It's essential to check for this to avoid filesystem corruption and to avoid
+  // accidentally opening an ext2/3/4 filesystem we don't understand, which would be disasterous.
+
+  if (Partition->FeaturesIncompat & ~gSupportedIncompatFeat) {
+    DEBUG ((DEBUG_ERROR, "[ext4] Unsupported features %lx\n",
+            Partition->FeaturesIncompat & ~gSupportedIncompatFeat));
+    return EFI_UNSUPPORTED;
+  }
+
+  // This should be removed once we add ext2/3 support in the future.
+  if ((Partition->FeaturesIncompat & EXT4_FEATURE_INCOMPAT_EXTENTS) == 0) {
+    return EFI_UNSUPPORTED;
+  }
+
+  // At the time of writing, it's the only supported checksum.
+  if (Partition->FeaturesCompat & EXT4_FEATURE_RO_COMPAT_METADATA_CSUM &&
+      Sb->s_checksum_type != EXT4_CHECKSUM_CRC32C) {
+    return EFI_UNSUPPORTED;
+  }
+
+  if (Partition->FeaturesIncompat & EXT4_FEATURE_INCOMPAT_CSUM_SEED) {
+    Partition->InitialSeed = Sb->s_checksum_seed;
+  } else {
+    Partition->InitialSeed = Ext4CalculateChecksum (Partition, Sb->s_uuid, 16, ~0U);
+  }
+
+  UnsupportedRoCompat = Partition->FeaturesRoCompat & ~gSupportedRoCompatFeat;
+
+  if (UnsupportedRoCompat != 0) {
+    DEBUG ((DEBUG_WARN, "[ext4] Unsupported ro compat %x\n", UnsupportedRoCompat));
+    Partition->ReadOnly = TRUE;
+  }
+
+  // gSupportedCompatFeat is documentation-only since we never need to access it.
+  // The line below avoids unused variable warnings.
+  (VOID)gSupportedCompatFeat;
+
+  DEBUG ((DEBUG_FS, "Read only = %u\n", Partition->ReadOnly));
+
+  Partition->BlockSize = (UINT32)LShiftU64 (1024, Sb->s_log_block_size);
+
+  // The size of a block group can also be calculated as 8 * Partition->BlockSize
+  if (Sb->s_blocks_per_group != 8 * Partition->BlockSize) {
+    return EFI_UNSUPPORTED;
+  }
+
+  Partition->NumberBlocks = EXT4_BLOCK_NR_FROM_HALFS (Partition, Sb->s_blocks_count, Sb->s_blocks_count_hi);
+  Partition->NumberBlockGroups = DivU64x32 (Partition->NumberBlocks, Sb->s_blocks_per_group);
+
+  DEBUG ((
+    DEBUG_FS,
+    "[ext4] Number of blocks = %lu\n[ext4] Number of block groups: %lu\n",
+    Partition->NumberBlocks,
+    Partition->NumberBlockGroups
+    ));
+
+  if (EXT4_IS_64_BIT (Partition)) {
+    Partition->DescSize = Sb->s_desc_size;
+  } else {
+    Partition->DescSize = EXT4_OLD_BLOCK_DESC_SIZE;
+  }
+
+  if (Partition->DescSize < EXT4_64BIT_BLOCK_DESC_SIZE && EXT4_IS_64_BIT (Partition)) {
+    // 64 bit filesystems need DescSize to be 64 bytes
+    return EFI_VOLUME_CORRUPTED;
+  }
+
+  if (!Ext4VerifySuperblockChecksum (Partition, Sb)) {
+    DEBUG ((DEBUG_ERROR, "[ext4] Bad superblock checksum %lx\n", Ext4CalculateSuperblockChecksum (Partition, Sb)));
+    return EFI_VOLUME_CORRUPTED;
+  }
+
+  NrBlocks = (UINTN)DivU64x32Remainder (
+                      MultU64x32 (Partition->NumberBlockGroups, Partition->DescSize),
+                      Partition->BlockSize,
+                      &NrBlocksRem
+                      );
+
+  if (NrBlocksRem != 0) {
+    NrBlocks++;
+  }
+
+  Partition->BlockGroups = Ext4AllocAndReadBlocks (Partition, NrBlocks, Partition->BlockSize == 1024 ? 2 : 1);
+
+  if (!Partition->BlockGroups) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  for (Index = 0; Index < Partition->NumberBlockGroups; Index++) {
+    Desc = Ext4GetBlockGroupDesc (Partition, Index);
+    if (!Ext4VerifyBlockGroupDescChecksum (Partition, Desc, Index)) {
+      DEBUG ((DEBUG_ERROR, "[ext4] Block group descriptor %u has an invalid checksum\n", Index));
+      return EFI_VOLUME_CORRUPTED;
+    }
+  }
+
+  // Note that the cast below is completely safe, because EXT4_FILE is a specialisation of EFI_FILE_PROTOCOL
+  Status = Ext4OpenVolume (&Partition->Interface, (EFI_FILE_PROTOCOL **)&Partition->Root);
+
+  return Status;
+}
+
+/**
+   Calculates the checksum of the given buffer.
+   @param[in]      Partition     Pointer to the opened EXT4 partition.
+   @param[in]      Buffer        Pointer to the buffer.
+   @param[in]      Length        Length of the buffer, in bytes.
+   @param[in]      InitialValue  Initial value of the CRC.
+
+   @return The checksum.
+**/
+UINT32
+Ext4CalculateChecksum (
+  IN CONST EXT4_PARTITION  *Partition,
+  IN CONST VOID            *Buffer,
+  IN UINTN                 Length,
+  IN UINT32                InitialValue
+  )
+{
+  if (!EXT4_HAS_METADATA_CSUM (Partition)) {
+    return 0;
+  }
+
+  switch (Partition->SuperBlock.s_checksum_type) {
+    case EXT4_CHECKSUM_CRC32C:
+      // For some reason, EXT4 really likes non-inverted CRC32C checksums, so we stick to that here.
+      return ~CalculateCrc32c(Buffer, Length, ~InitialValue);
+    default:
+      UNREACHABLE ();
+      return 0;
+  }
+}

Features/Ext4Pkg/Ext4Dxe/BlockGroup.c
@@ -1,0 +1,228 @@
+/** @file
+  Block group related routines
+
+  Copyright (c) 2021 Pedro Falcato All rights reserved.
+  Copyright (c) 2007 - 2018, Intel Corporation. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include "Ext4Dxe.h"
+
+/**
+   Retrieves a block group descriptor of the ext4 filesystem.
+
+   @param[in]  Partition      Pointer to the opened ext4 partition.
+   @param[in]  BlockGroup    Block group number.
+
+   @return A pointer to the block group descriptor.
+**/
+EXT4_BLOCK_GROUP_DESC *
+Ext4GetBlockGroupDesc (
+  IN EXT4_PARTITION  *Partition,
+  IN UINT32          BlockGroup
+  )
+{
+  // Maybe assert that the block group nr isn't a nonsense number?
+  return (EXT4_BLOCK_GROUP_DESC *)((CHAR8 *)Partition->BlockGroups + BlockGroup * Partition->DescSize);
+}
+
+/**
+   Reads an inode from disk.
+
+   @param[in]    Partition  Pointer to the opened partition.
+   @param[in]    InodeNum   Number of the desired Inode
+   @param[out]   OutIno     Pointer to where it will be stored a pointer to the read inode.
+
+   @return Status of the inode read.
+**/
+EFI_STATUS
+Ext4ReadInode (
+  IN EXT4_PARTITION  *Partition,
+  IN EXT4_INO_NR     InodeNum,
+  OUT EXT4_INODE     **OutIno
+  )
+{
+  UINT64                 InodeOffset;
+  UINT32                 BlockGroupNumber;
+  EXT4_INODE             *Inode;
+  EXT4_BLOCK_GROUP_DESC  *BlockGroup;
+  EXT4_BLOCK_NR          InodeTableStart;
+  EFI_STATUS             Status;
+
+  BlockGroupNumber = (UINT32)DivU64x64Remainder (
+                               InodeNum - 1,
+                               Partition->SuperBlock.s_inodes_per_group,
+                               &InodeOffset
+                               );
+
+  // Check for the block group number's correctness
+  if (BlockGroupNumber >= Partition->NumberBlockGroups) {
+    return EFI_VOLUME_CORRUPTED;
+  }
+
+  Inode = Ext4AllocateInode (Partition);
+
+  if (Inode == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  BlockGroup = Ext4GetBlockGroupDesc (Partition, BlockGroupNumber);
+
+  // Note: We'll need to check INODE_UNINIT and friends when/if we add write support
+
+  InodeTableStart = EXT4_BLOCK_NR_FROM_HALFS (
+                      Partition,
+                      BlockGroup->bg_inode_table_lo,
+                      BlockGroup->bg_inode_table_hi
+                      );
+
+  Status = Ext4ReadDiskIo (
+             Partition,
+             Inode,
+             Partition->InodeSize,
+             EXT4_BLOCK_TO_BYTES (Partition, InodeTableStart) + MultU64x32 (InodeOffset, Partition->InodeSize)
+             );
+
+  if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "[ext4] Error reading inode: status %x; inode offset %lx"
+      " inode table start %lu block group %lu\n",
+      Status,
+      InodeOffset,
+      InodeTableStart,
+      BlockGroupNumber
+      ));
+    FreePool (Inode);
+    return Status;
+  }
+
+  if (!Ext4CheckInodeChecksum (Partition, Inode, InodeNum)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "[ext4] Inode %llu has invalid checksum (calculated %x)\n",
+      InodeNum,
+      Ext4CalculateInodeChecksum (Partition, Inode, InodeNum)
+      ));
+    FreePool (Inode);
+    return EFI_VOLUME_CORRUPTED;
+  }
+
+  *OutIno = Inode;
+  return EFI_SUCCESS;
+}
+
+/**
+   Calculates the checksum of the block group descriptor for METADATA_CSUM enabled filesystems.
+   @param[in]      Partition       Pointer to the opened EXT4 partition.
+   @param[in]      BlockGroupDesc  Pointer to the block group descriptor.
+   @param[in]      BlockGroupNum   Number of the block group.
+
+   @return The checksum.
+**/
+STATIC
+UINT16
+Ext4CalculateBlockGroupDescChecksumMetadataCsum (
+  IN CONST EXT4_PARTITION         *Partition,
+  IN CONST EXT4_BLOCK_GROUP_DESC  *BlockGroupDesc,
+  IN UINT32                       BlockGroupNum
+  )
+{
+  UINT32  Csum;
+  UINT16  Dummy;
+
+  Dummy = 0;
+
+  Csum = Ext4CalculateChecksum (Partition, &BlockGroupNum, sizeof (BlockGroupNum), Partition->InitialSeed);
+  Csum = Ext4CalculateChecksum (Partition, BlockGroupDesc, OFFSET_OF (EXT4_BLOCK_GROUP_DESC, bg_checksum), Csum);
+  Csum = Ext4CalculateChecksum (Partition, &Dummy, sizeof (Dummy), Csum);
+  Csum =
+    Ext4CalculateChecksum (
+      Partition,
+      &BlockGroupDesc->bg_block_bitmap_hi,
+      Partition->DescSize - OFFSET_OF (EXT4_BLOCK_GROUP_DESC, bg_block_bitmap_hi),
+      Csum
+      );
+  return (UINT16)Csum;
+}
+
+/**
+   Calculates the checksum of the block group descriptor for GDT_CSUM enabled filesystems.
+   @param[in]      Partition       Pointer to the opened EXT4 partition.
+   @param[in]      BlockGroupDesc  Pointer to the block group descriptor.
+   @param[in]      BlockGroupNum   Number of the block group.
+
+   @return The checksum.
+**/
+STATIC
+UINT16
+Ext4CalculateBlockGroupDescChecksumGdtCsum (
+  IN CONST EXT4_PARTITION         *Partition,
+  IN CONST EXT4_BLOCK_GROUP_DESC  *BlockGroupDesc,
+  IN UINT32                       BlockGroupNum
+  )
+{
+  UINT16  Csum;
+  UINT16  Dummy;
+
+  Dummy = 0;
+
+  Csum = CalculateCrc16 (Partition->SuperBlock.s_uuid, 16, 0);
+  Csum = CalculateCrc16 (&BlockGroupNum, sizeof (BlockGroupNum), Csum);
+  Csum = CalculateCrc16 (BlockGroupDesc, OFFSET_OF (EXT4_BLOCK_GROUP_DESC, bg_checksum), Csum);
+  Csum = CalculateCrc16 (&Dummy, sizeof (Dummy), Csum);
+  Csum =
+    CalculateCrc16 (
+      &BlockGroupDesc->bg_block_bitmap_hi,
+      Partition->DescSize - OFFSET_OF (EXT4_BLOCK_GROUP_DESC, bg_block_bitmap_hi),
+      Csum
+      );
+  return Csum;
+}
+
+/**
+   Checks if the checksum of the block group descriptor is correct.
+   @param[in]      Partition       Pointer to the opened EXT4 partition.
+   @param[in]      BlockGroupDesc  Pointer to the block group descriptor.
+   @param[in]      BlockGroupNum   Number of the block group.
+
+   @return TRUE if checksum is correct, FALSE if there is corruption.
+**/
+BOOLEAN
+Ext4VerifyBlockGroupDescChecksum (
+  IN CONST EXT4_PARTITION         *Partition,
+  IN CONST EXT4_BLOCK_GROUP_DESC  *BlockGroupDesc,
+  IN UINT32                       BlockGroupNum
+  )
+{
+  if (!EXT4_HAS_METADATA_CSUM (Partition) && !EXT4_HAS_GDT_CSUM (Partition)) {
+    return TRUE;
+  }
+
+  return Ext4CalculateBlockGroupDescChecksum (Partition, BlockGroupDesc, BlockGroupNum) == BlockGroupDesc->bg_checksum;
+}
+
+/**
+   Calculates the checksum of the block group descriptor.
+   @param[in]      Partition       Pointer to the opened EXT4 partition.
+   @param[in]      BlockGroupDesc  Pointer to the block group descriptor.
+   @param[in]      BlockGroupNum   Number of the block group.
+
+   @return The checksum.
+**/
+UINT16
+Ext4CalculateBlockGroupDescChecksum (
+  IN CONST EXT4_PARTITION         *Partition,
+  IN CONST EXT4_BLOCK_GROUP_DESC  *BlockGroupDesc,
+  IN UINT32                       BlockGroupNum
+  )
+{
+  if (Partition->FeaturesRoCompat & EXT4_FEATURE_RO_COMPAT_METADATA_CSUM) {
+    return Ext4CalculateBlockGroupDescChecksumMetadataCsum (Partition, BlockGroupDesc, BlockGroupNum);
+  } else if (Partition->FeaturesRoCompat & EXT4_FEATURE_RO_COMPAT_GDT_CSUM) {
+    return Ext4CalculateBlockGroupDescChecksumGdtCsum (Partition, BlockGroupDesc, BlockGroupNum);
+  }
+
+  return 0;
+}

Features/Ext4Pkg/Ext4Dxe/Collation.c
@@ -1,0 +1,173 @@
+/** @file
+  Unicode collation routines
+
+  Copyright (c) 2021 Pedro Falcato All rights reserved.
+  Copyright (c) 2005 - 2017, Intel Corporation. All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <Uefi.h>
+
+#include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/MemoryAllocationLib.h>
+
+#include <Protocol/UnicodeCollation.h>
+
+STATIC EFI_UNICODE_COLLATION_PROTOCOL  *gUnicodeCollationInterface = NULL;
+
+/*
+ * Note: This code is heavily based on FatPkg's Unicode collation, since they seem to know what
+ * they're doing.
+ * PS: Maybe all this code could be put in a library? It looks heavily shareable.
+**/
+
+/**
+  Worker function to initialize Unicode Collation support.
+
+  It tries to locate Unicode Collation (2) protocol and matches it with current
+  platform language code.
+
+  @param[in]  DriverHandle         The handle used to open Unicode Collation (2) protocol.
+  @param[in]  ProtocolGuid         The pointer to Unicode Collation (2) protocol GUID.
+  @param[in]  VariableName         The name of the RFC 4646 or ISO 639-2 language variable.
+  @param[in]  DefaultLanguage      The default language in case the RFC 4646 or ISO 639-2 language is absent.
+
+  @retval EFI_SUCCESS          The Unicode Collation (2) protocol has been successfully located.
+  @retval Others               The Unicode Collation (2) protocol has not been located.
+
+**/
+STATIC
+EFI_STATUS
+Ext4InitialiseUnicodeCollationInternal (
+  IN EFI_HANDLE    DriverHandle,
+  IN EFI_GUID      *ProtocolGuid,
+  IN CONST CHAR16  *VariableName,
+  IN CONST CHAR8   *DefaultLanguage
+  )
+{
+  UINTN                           NumHandles;
+  EFI_HANDLE                      *Handles;
+  EFI_UNICODE_COLLATION_PROTOCOL  *Uci;
+  BOOLEAN                         Iso639Language;
+  CHAR8                           *Language;
+  EFI_STATUS                      RetStatus;
+  EFI_STATUS                      Status;
+  UINTN                           Idx;
+  CHAR8                           *BestLanguage;
+
+  Iso639Language = (BOOLEAN)(ProtocolGuid == &gEfiUnicodeCollationProtocolGuid);
+  RetStatus = EFI_UNSUPPORTED;
+  GetEfiGlobalVariable2 (VariableName, (VOID **)&Language, NULL);
+
+  Status = gBS->LocateHandleBuffer (
+                  ByProtocol,
+                  ProtocolGuid,
+                  NULL,
+                  &NumHandles,
+                  &Handles
+                  );
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  for (Idx = 0; Idx < NumHandles; Idx++) {
+    Status = gBS->OpenProtocol (
+                    Handles[Idx],
+                    ProtocolGuid,
+                    (VOID **)&Uci,
+                    DriverHandle,
+                    NULL,
+                    EFI_OPEN_PROTOCOL_GET_PROTOCOL
+                    );
+
+    if (EFI_ERROR (Status)) {
+      continue;
+    }
+
+    BestLanguage = GetBestLanguage (
+                     Uci->SupportedLanguages,
+                     Iso639Language,
+                     (Language == NULL) ? "" : Language,
+                     DefaultLanguage,
+                     NULL
+                     );
+    if (BestLanguage != NULL) {
+      FreePool (BestLanguage);
+      gUnicodeCollationInterface = Uci;
+      RetStatus = EFI_SUCCESS;
+      break;
+    }
+  }
+
+  if (Language != NULL) {
+    FreePool (Language);
+  }
+
+  FreePool (Handles);
+  return RetStatus;
+}
+
+/**
+   Initialises Unicode collation, which is needed for case-insensitive string comparisons
+   within the driver (a good example of an application of this is filename comparison).
+
+   @param[in]      DriverHandle    Handle to the driver image.
+
+   @retval EFI_SUCCESS   Unicode collation was successfully initialised.
+   @retval !EFI_SUCCESS  Failure.
+**/
+EFI_STATUS
+Ext4InitialiseUnicodeCollation (
+  EFI_HANDLE  DriverHandle
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = EFI_UNSUPPORTED;
+
+  //
+  // First try to use RFC 4646 Unicode Collation 2 Protocol.
+  //
+  Status = Ext4InitialiseUnicodeCollationInternal (
+             DriverHandle,
+             &gEfiUnicodeCollation2ProtocolGuid,
+             L"PlatformLang",
+             (CONST CHAR8 *)PcdGetPtr (PcdUefiVariableDefaultPlatformLang)
+             );
+  //
+  // If the attempt to use Unicode Collation 2 Protocol fails, then we fall back
+  // on the ISO 639-2 Unicode Collation Protocol.
+  //
+  if (EFI_ERROR (Status)) {
+    Status = Ext4InitialiseUnicodeCollationInternal (
+               DriverHandle,
+               &gEfiUnicodeCollationProtocolGuid,
+               L"Lang",
+               (CONST CHAR8 *)PcdGetPtr (PcdUefiVariableDefaultLang)
+               );
+  }
+
+  return Status;
+}
+
+/**
+   Does a case-insensitive string comparison. Refer to EFI_UNICODE_COLLATION_PROTOCOL's StriColl
+   for more details.
+
+   @param[in]      Str1   Pointer to a null terminated string.
+   @param[in]      Str2   Pointer to a null terminated string.
+
+   @retval 0   Str1 is equivalent to Str2.
+   @retval >0  Str1 is lexically greater than Str2.
+   @retval <0  Str1 is lexically less than Str2.
+**/
+INTN
+Ext4StrCmpInsensitive (
+  IN CHAR16  *Str1,
+  IN CHAR16  *Str2
+  )
+{
+  return gUnicodeCollationInterface->StriColl (gUnicodeCollationInterface, Str1, Str2);
+}

Features/Ext4Pkg/Ext4Dxe/Crc16.c
@@ -1,0 +1,75 @@
+/** @file
+  CRC16 calculation routines.
+
+  Copyright (c) 2021 Pedro Falcato All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <Uefi.h>
+
+STATIC CONST UINT16  gCrc16LookupTable[256] =
+{
+  0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
+  0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7,
+  0x0919, 0x1890, 0x2a0b, 0x3b82, 0x4f3d, 0x5eb4, 0x6c2f, 0x7da6,
+  0x8551, 0x94d8, 0xa643, 0xb7ca, 0xc375, 0xd2fc, 0xe067, 0xf1ee,
+  0x1232, 0x03bb, 0x3120, 0x20a9, 0x5416, 0x459f, 0x7704, 0x668d,
+  0x9e7a, 0x8ff3, 0xbd68, 0xace1, 0xd85e, 0xc9d7, 0xfb4c, 0xeac5,
+  0x1b2b, 0x0aa2, 0x3839, 0x29b0, 0x5d0f, 0x4c86, 0x7e1d, 0x6f94,
+  0x9763, 0x86ea, 0xb471, 0xa5f8, 0xd147, 0xc0ce, 0xf255, 0xe3dc,
+  0x2464, 0x35ed, 0x0776, 0x16ff, 0x6240, 0x73c9, 0x4152, 0x50db,
+  0xa82c, 0xb9a5, 0x8b3e, 0x9ab7, 0xee08, 0xff81, 0xcd1a, 0xdc93,
+  0x2d7d, 0x3cf4, 0x0e6f, 0x1fe6, 0x6b59, 0x7ad0, 0x484b, 0x59c2,
+  0xa135, 0xb0bc, 0x8227, 0x93ae, 0xe711, 0xf698, 0xc403, 0xd58a,
+  0x3656, 0x27df, 0x1544, 0x04cd, 0x7072, 0x61fb, 0x5360, 0x42e9,
+  0xba1e, 0xab97, 0x990c, 0x8885, 0xfc3a, 0xedb3, 0xdf28, 0xcea1,
+  0x3f4f, 0x2ec6, 0x1c5d, 0x0dd4, 0x796b, 0x68e2, 0x5a79, 0x4bf0,
+  0xb307, 0xa28e, 0x9015, 0x819c, 0xf523, 0xe4aa, 0xd631, 0xc7b8,
+  0x48c8, 0x5941, 0x6bda, 0x7a53, 0x0eec, 0x1f65, 0x2dfe, 0x3c77,
+  0xc480, 0xd509, 0xe792, 0xf61b, 0x82a4, 0x932d, 0xa1b6, 0xb03f,
+  0x41d1, 0x5058, 0x62c3, 0x734a, 0x07f5, 0x167c, 0x24e7, 0x356e,
+  0xcd99, 0xdc10, 0xee8b, 0xff02, 0x8bbd, 0x9a34, 0xa8af, 0xb926,
+  0x5afa, 0x4b73, 0x79e8, 0x6861, 0x1cde, 0x0d57, 0x3fcc, 0x2e45,
+  0xd6b2, 0xc73b, 0xf5a0, 0xe429, 0x9096, 0x811f, 0xb384, 0xa20d,
+  0x53e3, 0x426a, 0x70f1, 0x6178, 0x15c7, 0x044e, 0x36d5, 0x275c,
+  0xdfab, 0xce22, 0xfcb9, 0xed30, 0x998f, 0x8806, 0xba9d, 0xab14,
+  0x6cac, 0x7d25, 0x4fbe, 0x5e37, 0x2a88, 0x3b01, 0x099a, 0x1813,
+  0xe0e4, 0xf16d, 0xc3f6, 0xd27f, 0xa6c0, 0xb749, 0x85d2, 0x945b,
+  0x65b5, 0x743c, 0x46a7, 0x572e, 0x2391, 0x3218, 0x0083, 0x110a,
+  0xe9fd, 0xf874, 0xcaef, 0xdb66, 0xafd9, 0xbe50, 0x8ccb, 0x9d42,
+  0x7e9e, 0x6f17, 0x5d8c, 0x4c05, 0x38ba, 0x2933, 0x1ba8, 0x0a21,
+  0xf2d6, 0xe35f, 0xd1c4, 0xc04d, 0xb4f2, 0xa57b, 0x97e0, 0x8669,
+  0x7787, 0x660e, 0x5495, 0x451c, 0x31a3, 0x202a, 0x12b1, 0x0338,
+  0xfbcf, 0xea46, 0xd8dd, 0xc954, 0xbdeb, 0xac62, 0x9ef9, 0x8f70
+};
+
+/**
+   Calculates the CRC16 checksum of the given buffer.
+
+   @param[in]      Buffer        Pointer to the buffer.
+   @param[in]      Length        Length of the buffer, in bytes.
+   @param[in]      InitialValue  Initial value of the CRC.
+
+   @return The CRC16 checksum.
+**/
+UINT16
+CalculateCrc16 (
+  IN CONST VOID  *Buffer,
+  IN UINTN       Length,
+  IN UINT16      InitialValue
+  )
+{
+  CONST UINT8  *Buf;
+  UINT16       Crc;
+
+  Buf = Buffer;
+
+  Crc = ~InitialValue;
+
+  while (Length-- != 0) {
+    Crc = gCrc16LookupTable[(Crc & 0xFF) ^ *(Buf++)] ^ (Crc >> 8);
+  }
+
+  return ~Crc;
+}

heatd/edk2-platforms

commit 304f4805813080cfe8f787a0e845697dff2ca78f

Ext4Pkg: Fix division by zero by adding check for s_inodes_per_group
Superblock s_inodes_per_group field can't be zero, it leads to division
by zero in BlockGroup routine Ext4ReadInode

Cc: Marvin Häuser <mhaeuser@posteo.de>
Cc: Pedro Falcato <pedro.falcato@gmail.com>
Cc: Vitaly Cheptsov <vit9696@protonmail.com>
Fixes: d9ceedca6c8f ("Ext4Pkg: Add Ext4Dxe driver.")
Signed-off-by: Savva Mitrofanov <savvamtr@gmail.com>
Reviewed-by: Pedro Falcato <pedro.falcato@gmail.com>
Reviewed-by: Marvin Häuser <mhaeuser@posteo.de>

Features/Ext4Pkg/Ext4Dxe/Superblock.c
@@ -243,6 +243,11 @@ Ext4OpenSuperblock (
 
   DEBUG ((DEBUG_FS, "Read only = %u\n", Partition->ReadOnly));
 
+  if (Sb->s_inodes_per_group == 0) {
+    DEBUG ((DEBUG_ERROR, "[ext4] Inodes per group can not be zero\n"));
+    return EFI_VOLUME_CORRUPTED;
+  }
+
   Partition->BlockSize = (UINT32)LShiftU64 (1024, Sb->s_log_block_size);
 
   // The size of a block group can also be calculated as 8 * Partition->BlockSize